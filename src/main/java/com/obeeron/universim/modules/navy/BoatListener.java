60e28f4fefe9e7010e2e9888ef2636311a90beb1
LangArthur <arthur.lang.pro@gmail.com>
2024-08-18 14:54:40 +0100

    fix: resolve drop of boat

--- src/main/java/com/obeeron/universim/modules/navy/BoatListener.java
+++ src/main/java/com/obeeron/universim/modules/navy/BoatListener.java
@@ -2,15 +2,22 @@ package com.obeeron.universim.modules.navy;
 
 import java.util.HashMap;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Boat.Type;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 import com.obeeron.universim.UVSCore;
+import com.obeeron.universim.modules.universimItems.UnivItemManager;
 
 public class BoatListener implements Listener {
     private HashMap<String, String> waitingEvent = new HashMap<String, String>(20);
@@ -52,6 +59,23 @@ public class BoatListener implements Listener {
                 }
                 waitingEvent.remove(playerId);
             }
+        }
+    }
+
+    @EventHandler
+    public void DropBoat(VehicleDestroyEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        if (event.getVehicle().getType() == EntityType.BOAT) {
+            Boat boat = (Boat)event.getVehicle();
+            Type type = boat.getBoatType();
+            String univId = type.toString().toLowerCase() + "_" + boat.getCustomName();
+            ItemStack item = UnivItemManager.getInstance().getUnivItem(UVSCore.univNSK(univId)); ;
+            Location loc1 = boat.getLocation();
+            loc1.getWorld().dropItem(loc1, item);
+            event.setCancelled(true);
+            boat.remove();
         }
     }
 }
